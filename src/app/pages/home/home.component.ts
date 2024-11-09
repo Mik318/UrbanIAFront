aadf0b2abe2dde2a32d98efc7303017b42ecdbfb
mike <lokilskdij@gmail.com>
2024-11-09 16:13:20 -0600

    First commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardFooter,
+  MatCardHeader,
+  MatCardModule
+} from "@angular/material/card";
+import {MatDivider, MatDividerModule} from "@angular/material/divider";
+import {MatProgressBar, MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatDivider,
+    MatCardActions,
+    MatCardFooter,
+    MatProgressBar,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatProgressBarModule
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
