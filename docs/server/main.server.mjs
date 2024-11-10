515079300c86f5cc98c9c93fb81c8f8406d3e2ce
mike <lokilskdij@gmail.com>
2024-11-10 13:34:14 -0600

    mas cambios

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-IJBXUD2V.mjs";import"./chunk-EICN6D2K.mjs";import"./chunk-73MH5XAC.mjs";import"./chunk-P3GKXXW4.mjs";import"./chunk-2QNQW5IT.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
